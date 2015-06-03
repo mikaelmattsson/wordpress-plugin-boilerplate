1b570fd710a822a7d37db9fe928559160e2656de
Mikael Mattsson <mikael@weblyan.se>
2015-06-03 10:20:10 +0200

    Update wordpress-plugin-boilerplate.php

--- wordpress-plugin-boilerplate.php
+++ wordpress-plugin-boilerplate.php
@@ -11,20 +11,13 @@
 
 /**
  * @wordpress-plugin
- * Plugin URI: http://wallmanderco.github.io/elasticsearch-indexer/
- * Plugin Name: Elasticsearch Indexer
- * Description: Elasticsearch indexer for WordPress and WooCommerce
- * Version: 1.2.4
+ * Plugin URI: https://github.com/mikaelmattsson/wordpress-plugin-boilerplate
+ * Plugin Name: Wordpress Plugin Boilerplate
+ * Description: A boilerplate for WordPress plugins that uses composer.
+ * Version: 1.2.3
  * Author: Mikael Mattsson
- * Text Domain: elasticsearch-indexer
+ * Text Domain: wordpress-plugin-boilerplate
  */
-if (defined('DISABLE_ES') && DISABLE_ES) {
-    return;
-}
-
-define('ESI_PLUGINFILE', __FILE__);
-define('ESI_PATH', dirname(ESI_PLUGINFILE).'/');
-define('ESI_URL', plugins_url('/', __FILE__));
 
 require_once ESI_PATH.'vendor/autoload.php';
 
